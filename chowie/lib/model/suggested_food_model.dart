e22e998a743769b956e5914a42b8a1addd70b9b0
softcoder404 <daseeker9@gmail.com>
2020-04-07 10:49:33 +0100

    suggested food model created

---
+++ chowie/lib/model/suggested_food_model.dart
@@ -0,0 +1,10 @@
+class SuggestedFoodModel{
+  final imageUrl;
+  final name;
+  final List<String> procedure;
+  SuggestedFoodModel({
+    this.imageUrl,
+    this.name,
+    this.procedure
+  });
+}
